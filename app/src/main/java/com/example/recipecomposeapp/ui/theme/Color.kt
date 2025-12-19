1a7e3ddf7f6bd0c79ee6377cb8d55d9d61be78a2
kuleshow <kuleshov995@yandex.ru>
2025-12-19 23:26:52 +0300

    initial commit

---
+++ app/src/main/java/com/example/recipecomposeapp/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.example.recipecomposeapp.ui.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val mainBlackColor = Color(0xFF1E1E1E)
+
+// светлая тема
+val  background = Color(0xFFF3EDF5)
+val mainTitleColor = Color(0xFF1E1E1E)
+val subtitileGrayColor = Color(0xFF8C8C8C)
+
+//темная тема
+val  backgroundDark = Color(0xFF121212)
+val mainTitleColorDark = Color(0xFFEAEAEA)
+val subtitileGrayColorDark = Color(0xFFD1D1D1)
