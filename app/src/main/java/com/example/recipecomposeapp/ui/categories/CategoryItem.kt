19870a72718305f24023283bcbfe44766c6de830
kuleshow <kuleshov995@yandex.ru>
2026-02-22 22:04:01 +0300

    RCA-14: доработал экран категорий

---
+++ app/src/main/java/com/example/recipecomposeapp/ui/categories/CategoryItem.kt
@@ -0,0 +1,60 @@
+package com.example.recipecomposeapp.ui.categories
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.recipecomposeapp.R
+import com.example.recipecomposeapp.ui.categories.model.CategoryUiModel
+import com.example.recipecomposeapp.ui.theme.Dimensions
+import com.example.recipecomposeapp.ui.theme.RecipesAppTypography
+
+@Composable
+fun CategoryItem(model: CategoryUiModel, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .height(220.dp)
+            .width(150.dp),
+        onClick = onClick,
+        colors = CardDefaults
+            .cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        shape = RoundedCornerShape(Dimensions.shapeMedium)
+    ) {
+        Column {
+            AsyncImage(
+                model = model.imageUrl,
+                contentDescription = "фон",
+                contentScale = ContentScale.Crop,
+                error = painterResource(R.drawable.img_error),
+                placeholder = painterResource(R.drawable.img_placeholder),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.7f)
+            )
+            Text(
+                text = model.title,
+                modifier = Modifier.padding(Dimensions.paddingSmall),
+                color = MaterialTheme.colorScheme.primary,
+                style = RecipesAppTypography.bodyMedium
+            )
+            Text(
+                text = model.description,
+                modifier = Modifier.padding(Dimensions.paddingSmall),
+                color = MaterialTheme.colorScheme.primary,
+                style = RecipesAppTypography.bodySmall
+            )
+        }
+    }
+}
